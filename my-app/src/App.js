824f9ae14c35a66cfd604bf14c4bdb0d2dc940b5
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-07 19:27:08 +0300

    task/login/register in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import {React,Route,Routes,Link} from "react-router-dom";
+import Login from './components/Login';
+import Task from './components/Task';
+import Register from './components/Register';
 function App() {
   return (
-    <div className="App">
-     vvvvvvvv
+    <div>
+     <Routes>
+     <Route path="/login" component={Login}/>
+     <Route path="/register" component={Register}/>
+     <Route path="/task" component={Task}/>
+     </Routes>
+     <Link to="/register" >Register</Link>
+     <Link to="/login" >Login</Link>
+     <Link to="/task" >Task</Link>
     </div>
+
   );
 }
 
