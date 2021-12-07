fb16b62497fda10ea9514fbcd63f0bbcb3a11e56
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-07 20:25:48 +0300

    reducers-index.js finsh

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,4 +1,13 @@
 import {createStore,combineReducers} from 'redud';
 import {composeWithDevTools} from "redux-devtools-extension";
+import singnIn from './login';
 
+
+const reducers = combineReducers({singnIn});
+
+const store =  ()=>{
+    return createStore(reducers,composeWithDevTools());
+};
+
+export default store();
 
