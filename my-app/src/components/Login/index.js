900fbcbf5f7d571cd2cbd58bc66b03847db05db8
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-07 19:58:00 +0300

    reducers-login finsh

--- my-app/src/components/Login/index.js
+++ my-app/src/components/Login/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import axios from "axios";
 
 const Login = () => {
     return (
---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,4 @@
+import {createStore,combineReducers} from 'redud';
+import {composeWithDevTools} from "redux-devtools-extension";
+
+
